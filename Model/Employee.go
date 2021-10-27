4bcfdb01c9f1c0bc6d7bafa05a35391e0b036772
326487982@qq.com <326487982@qq.com>
2021-10-27 19:29:08 +0800

    update model

--- Model/Employee.go
+++ Model/Employee.go
@@ -1,8 +1,90 @@
 package model
-type Employee struct{
-	itsEmpid int
-	itsName string
+
+import "time"
+
+type Employee struct {
+	itsEmpid   int
+	itsName    string
 	itsAddress string
 
-	
+	itsClassification PaymentClassification
+	itsSchedule       PaymentSchedule
+	itsPaymentMethod  PaymentMethod
+	itsAffiliation    Affiliation
+}
+
+func (employee Employee) SetEmployee(empid int, name string, address string) {
+	employee.itsEmpid = empid
+	employee.itsName = name
+	employee.itsAddress = address
+}
+
+func (employee Employee) SetClassification(pc PaymentClassification) {
+	employee.itsClassification = pc
+}
+
+func (employee Employee) SetSchedule(ps PaymentSchedule) {
+	employee.itsSchedule = ps
+}
+
+func (employee Employee) SetMethod(pm PaymentMethod) {
+	employee.itsPaymentMethod = pm
+}
+
+func (employee Employee) GetName() string {
+	return employee.itsName
+}
+
+func (employee Employee) GetClassification() PaymentClassification {
+	return employee.itsClassification
+}
+
+func (employee Employee) GetSchedule() PaymentSchedule {
+	return employee.itsSchedule
+}
+
+func (employee Employee) GetMethod() PaymentMethod {
+	return employee.itsPaymentMethod
+}
+
+func (employee Employee) SetAffiliation(af Affiliation) {
+	employee.itsAffiliation = af
+}
+
+func (employee Employee) GetAffiliation() Affiliation {
+	return employee.itsAffiliation
+}
+
+func (employee Employee) GetEmpid() int {
+	return employee.itsEmpid
+}
+func (employee Employee) SetName(name string) {
+	employee.itsName = name
+}
+
+func (employee Employee) SetAddress(address string) {
+	employee.itsAddress = address
+}
+
+func (employee Employee) GetAddress() string {
+	return employee.itsAddress
+}
+
+func (employee Employee) IsPayDate(payDate time.Time) bool {
+	return employee.itsSchedule.IsPaydate(payDate)
+}
+
+func (employee Employee) GetPayPeriodStartDate(payPeriodEndDate time.Time) time.Time{
+	return employee.itsSchedule.GetPayPeriodStartDate(payPeriodEndDate)
+}
+
+func (employee Employee) Payday(Paycheck pc) {
+	grossPay := itsClassification.CalculatePay(pc)
+	deductions := itsAffiliation.CalculateDeductions(pc)
+	netPay := grossPay - deductions
+	pc.SetGrossPay(grossPay)
+	pc.SetDeductions(deductions)
+	pc.SetNetPay(netPay)
+	itsPaymentMethod.Pay(pc)
+
 }
